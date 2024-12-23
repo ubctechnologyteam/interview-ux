8d64726708ff8cb95dfd902c5d793933de156d96
ubctechnologyteam <ubctechnologyteam@gmail.com>
2024-12-23 15:30:58 -0500

    ReactJs app setup
    * ReactJs app setup
    
    * Update README.md

---
+++ ubc-test/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './main.scss';
+
+function App() {
+  return (
+    <div className="container">
+        <p>
+          Test App
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
