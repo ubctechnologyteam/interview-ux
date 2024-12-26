1bf144b05a0d8939a5ef710b6e08bdbbfefd1c91
Mahesh Palghadmal <mahesh_pal4@hotmail.com>
2024-12-26 21:06:40 +0530

    Initial commit
    Initial commit

--- ubc-test/src/App.tsx
+++ ubc-test/src/App.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
 import './main.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
     <div className="container">
-        <p>
-          Test App
-        </p>
+        <header>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="container-fluid">
+           <div class="navbar-brand logo">
+             <i class="fa-brands fa-google"></i>
+         </div>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav margin-right-auto margin-right-auto mb-2 mb-lg-0">
+               <li class="nav-item">
+                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Link</a>
+               </li>
+               <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                   Dropdown
+                 </a>
+                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                   <li><a class="dropdown-item" href="#">Action</a></li>
+                   <li><a class="dropdown-item" href="#">Another action</a></li>
+                   <li><hr class="dropdown-divider"></li>
+                   <li><a class="dropdown-item" href="#">Something else here</a></li>
+                 </ul>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+               </li>
+             </ul>
+             
+           </div>
+         </div>
+       </nav>
+   </header>
+     <div class="card-container">
+       <div class="card cardsBg">
+           <i class="fa-brands fa-product-hunt"></i>
+           <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+         <div class="card cardsBg">
+           <i class="fa-brands fa-product-hunt"></i>
+           <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+         <div class="card cardsBg">
+           <i class="fa-brands fa-product-hunt"></i>
+           <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+     </div>
+     <footer>
+        <div class="social-media-content">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-youtube"></i>
+        </div>
+      </footer>
         
     </div>
   );
